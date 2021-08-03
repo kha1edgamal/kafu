7df5ad6bf9eb3d450abb5ea12c809d82827b1a09
Khalid Jamal <5aled.gamal231@gmail.com>
2021-08-04 00:41:11 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(document).ready(function () {
         /*Smooth Scroll*/
      $('a[href*="#"]').smoothscroll();
+     //--Owl Init --
+     $('.join-slider').owlCarousel({
+        animateOut: 'fadeOut',
+        loop:false,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $('.statics-slider').owlCarousel({
+        loop:false,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
 });
